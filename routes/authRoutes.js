941e50deeb53119b393fcbfe3b3a1b6ce18562a8
timothee-piotre <155533950+timothee-piotre@users.noreply.github.com>
2024-08-28 11:05:17 +0200

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,10 @@
 // routes/authRoutes.js
 const express = require('express');
-const { register, login } = require('../controllers/authController');
+const { register, login } = require('controllers/authController');
 
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login', login);
+router.post('register', register);
+router.post('index', login);
 
 module.exports = router;
